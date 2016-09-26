ec43fdb6e6aa502d6b96c0ee527985896aec0170
Magnus Jerre <magnus.jerre@ciber.com>
2016-09-26 12:16:24 +0200

    Endrer slack-meldingen

--- public/js/index.js
+++ public/js/index.js
@@ -246,7 +246,7 @@ $(document).ready(function(){
 					type: 'POST',
 					url: 'https://hooks.slack.com/services/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',
 					crossDomain: true,
-					data: '{"text":"Nytt brygg er klart! Bryggnavn: ' + bryggdata.bryggnavn +'"}',
+					data: '{"text":"Nytt brygg er klart! *' + bryggdata.bryggnavn +'*\nVurder den på www.jerre.no"}',
 					dataType: 'json',
 					success: function(responseData, textStatus, jqXHR) {
 						console.log("Successfully posted to slack")
